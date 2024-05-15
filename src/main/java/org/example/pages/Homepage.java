68e545f20df12176e0919ec91a8483afff2e3f9c
CodeMahmoud <mahmoudcode913@gmail.com>
2024-05-15 11:44:24 -0500

    Pushed changes to repo

---
+++ src/main/java/org/example/pages/Homepage.java
@@ -0,0 +1,13 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+
+public class Homepage {
+
+    // By
+    By showActivityBtn = By.xpath("/html/body/yelp-react-root/div[1]/div[3]/div/div/div[3]/button");
+
+    // Methods or actions to perform
+
+
+}
