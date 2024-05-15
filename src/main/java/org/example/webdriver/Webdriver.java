1a63890de4f16b6fe9e13ea04d89835f6223e285
CodeMahmoud <mahmoudcode913@gmail.com>
2024-05-15 11:52:15 -0500

    added project files to repo

---
+++ src/main/java/org/example/webdriver/Webdriver.java
@@ -0,0 +1,8 @@
+package org.example.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Webdriver {
+    WebDriver driver = new ChromeDriver();
+}
